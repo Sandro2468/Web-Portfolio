8499fffee878f2815852e161eebdcdd788b505be
Sandro2468 <laksandror@gmail.com>
2024-03-25 22:57:18 +0700

    first commit

---
+++ constants/index.tsx
@@ -0,0 +1,144 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Html 5",
+    image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Css",
+    image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Postgres",
+    image: "/postges.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "JavaScript",
+    image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "Tailwind Css",
+    image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "React",
+    image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Redux",
+    image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    name: "TypeScript",
+    image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Next js 13",
+    image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Framer Motion",
+    image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Stripe Payment",
+    image: "/stripe.webp",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Node js",
+    image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Mongo db",
+    image: "/mongodb.png",
+    width: 40,
+    height: 40,
+  },
+];
+
+export const Socials = [
+  {
+    name: "Discord",
+    src: "/instagram.svg",
+  },
+  {
+    name: "Facebook",
+    src: "/facebook.svg",
+  },
+  {
+    name: "Instagram",
+    src: "/discord.svg",
+  },
+];
+export const Projects = [
+  {
+    title: "Restaurant_KW",
+    text: "Delightful online food ordering experience with variety.",
+    src: "/project2.png",
+  },
+  {
+    title:
+      "SplitBill: Bill Buddy",
+    text: "Bill Buddy: Simplify bill splitting online with ease.",
+    src: "/project3.png",
+  },
+  {
+    title: "Modern Nextjs Portfolio",
+    text: "Modern Next.js Portfolio is a polished web exhibit crafted with Next.js, showcasing a curated selection of projects and skills in an elegant and professional manner.",
+    src: "/WebPortfolio.png",
+  },
+  {
+    title: "bukangapakhe  Website",
+    text: "This application is an eCommerce-themed web app designed for educational purposes, leveraging cutting-edge technology and the Nextjs framework",
+    src: "/Matrix.png",
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/my-skills",
+    icon: RxPerson,
+    link: "/my-skills",
+  },
+  {
+    name: "/my-projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+  {
+    name: "/contact-me",
+    icon: RxClipboard,
+    link: "/contact-me",
+  },
+];
